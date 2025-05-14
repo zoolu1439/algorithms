c38a4c506921982bbb548685bf3c3046e2126837
zoolu1439 <bharat639@gmail.com>
2025-05-14 14:52:46 -0500

    Update QuickSort.java - to latest functions in Java 21

--- src/main/java/com/bharat/learning/algorithms/QuickSort.java
+++ src/main/java/com/bharat/learning/algorithms/QuickSort.java
@@ -1,68 +1,48 @@
 package com.bharat.learning.algorithms;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Quicksort
  * Time complexity 
- *      worst case -> n^2
- *      best case  -> nlogn
+ *      Worst case -> O(n^2)
+ *      Best case  -> O(n log n)
  * 
  */
-public class QuickSort 
-{
-    public static void main( String[] args )
-    {
-        
-        int[] unsortedArray = {2,3,4,5,7,97,1,101,8,10};
-        int startIndex = 0;
-        int endIndex = unsortedArray.length-1;
-        quicksort(unsortedArray,startIndex, endIndex);
-        System.out.println(Arrays.toString(unsortedArray));
+public class QuickSort {
+    public static void main(String[] args) {
+        List<Integer> unsortedList = List.of(2, 3, 4, 5, 7, 97, 1, 101, 8, 10);
+        List<Integer> mutableList = new ArrayList<>(unsortedList); // Make it mutable for sorting
+        quicksort(mutableList, 0, mutableList.size() - 1);
+        System.out.println(mutableList);
     }
 
-	private static void quicksort(int[] unsortedArray, int startIndex, int endIndex) {
-		if(startIndex<endIndex) 
-		{
-			
-			int partitionIndex = partition(unsortedArray,startIndex,endIndex);
-			quicksort(unsortedArray, startIndex, partitionIndex);
-			quicksort(unsortedArray, partitionIndex+1, endIndex);
-		}
-		
-	}
+    private static void quicksort(List<Integer> list, int startIndex, int endIndex) {
+        if (startIndex < endIndex) {
+            int partitionIndex = partition(list, startIndex, endIndex);
+            quicksort(list, startIndex, partitionIndex - 1);
+            quicksort(list, partitionIndex + 1, endIndex);
+        }
+    }
 
-	private static int partition(int[] unsortedArray, int startIndex, int endIndex) {
-		// TODO Auto-generated method stub
-			int i=startIndex;
-			int j=endIndex;
-			int pivot = unsortedArray[startIndex];
-			while(i<j) {
-				while(unsortedArray[i]<=pivot)
-				 
-				{
-					i++;
-				}
-				
-				while(unsortedArray[j]>pivot)
-					 
-				{
-					j--;
-				}
-				if(i<j) {
-					swap(unsortedArray,i,j);
-				}
-				
-			}
-			
-			swap(unsortedArray,startIndex,j);
-		
-		return j;
-	}
-
-	private static void swap(int[] unsortedArray, int i, int j) {
-		int temp = unsortedArray[i];
-		unsortedArray[i]=unsortedArray[j];
-		unsortedArray[j]=temp;
-	}
+    private static int partition(List<Integer> list, int startIndex, int endIndex) {
+        int pivot = list.get(startIndex);
+        int i = startIndex;
+        int j = endIndex;
+        while (i < j) {
+            while (i < list.size() && list.get(i) <= pivot) {
+                i++;
+            }
+            while (j >= 0 && list.get(j) > pivot) {
+                j--;
+            }
+            if (i < j) {
+                Collections.swap(list, i, j);
+            }
+        }
+        Collections.swap(list, startIndex, j);
+        return j;
+    }
 }
